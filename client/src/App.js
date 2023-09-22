d328aeb19a958ef014ad7eb8d589a5d468ac0c4f
Skye <hkim7095@gmail.com>
2023-09-22 17:44:55 +0900

    PriceCal + PageStep, done

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import SummaryPage from './pages/SummaryPage';
 import OrderPage from './pages/OrderPage';
+import CompletePage from './pages/CompletePage';
+import { useState } from 'react';
 
 function App() {
+
+  const [step, setStep] = useState(0)
+
   return (
     <div className="App">
-      <OrderPage />
-      <SummaryPage />
+      {step === 0 && <OrderPage setStep={setStep} />}
+      {step === 1 && <SummaryPage setStep={setStep} />}
+      {step === 2 && <CompletePage setStep={setStep} />}
     </div>
   );
 }
--- client/src/pages/CompletePage/index.js
+++ client/src/pages/CompletePage/index.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { OrderContext } from '../../context/OrderContext'
+import axios from 'axios'
 
-const CompletePage = () => {
-  return (
-    <div>CompletePage</div>
-  )
+const CompletePage = ({setStep}) => {
+
+  const [orderHistory, setOrderHistory] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [orderData] = useContext(OrderContext)
+
+  useEffect(() => {
+    orderCompleted(orderData)
+  }, [orderData])
+  
+  const orderCompleted = async (orderData) => {
+    try {
+      const res = await axios.post('http://localhost:4000/order', orderData)
+      
+      setOrderHistory(res.data)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const orderTable = orderHistory.map((item) => (
+    <tr key={item.orderNumber}>
+      <td>{item.orderNumber}</td>
+      <td>{item.price}</td>
+    </tr>
+  ))
+
+  if(loading) {
+    return (<div>...loading</div>)
+  }else{
+    return (
+      <div style={{textAlign: 'center'}}>
+        <h2>주문 성공했습니다.</h2>
+        <h3>지금까지 모든 주문</h3>
+        <table style={{margin: 'auto'}}>
+          <tbody>
+            <tr>
+              <th>Number</th>
+              <th>Price</th>
+            </tr>
+            {orderTable}
+          </tbody>
+        </table>
+        <br />
+        <button onClick={() => setStep(0)}>
+          첫 페이지로
+        </button>
+      </div>
+    )
+  }
 }
 
 export default CompletePage
